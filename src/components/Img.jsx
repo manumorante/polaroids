2433b2f199769443308df8d0bbfe7035dedc3f5a
Manu Morante <manu@estadologico.com>
2023-06-15 18:57:49 +0200

    Update

--- src/components/Img.jsx
+++ src/components/Img.jsx
@@ -1,7 +1,7 @@
 export default function Img({ index }) {
   return (
     <img
-      className='Img w-full h-auto aspect-square object-cover rounded-sm dark:contrast-50 dark:saturate-100'
+      className='Img w-full h-auto aspect-square object-cover rounded-sm'
       src={`https://picsum.photos/300?random=${index}`}
     />
   )
